607b3050bb145e3eb064eb6e940c4fc0e8370730
M.Syamsandi <zxvcklh@gmail.com>
2024-07-25 09:03:09 +0700

    add crud polyclinics + test role

---
+++ src/main/java/com/syamsandi/java_rs_rawat_jalan/model/PolyclinicResponse.java
@@ -0,0 +1,19 @@
+package com.syamsandi.java_rs_rawat_jalan.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PolyclinicResponse {
+
+  private UUID id;
+
+  private String name;
+}
---
+++ src/main/java/com/syamsandi/java_rs_rawat_jalan/service/PolyclinicService.java
@@ -0,0 +1,24 @@
+package com.syamsandi.java_rs_rawat_jalan.service;
+
+import com.syamsandi.java_rs_rawat_jalan.entity.User;
+import com.syamsandi.java_rs_rawat_jalan.model.PolyclinicRequest;
+import com.syamsandi.java_rs_rawat_jalan.model.PolyclinicResponse;
+import com.syamsandi.java_rs_rawat_jalan.model.RoleRequest;
+import com.syamsandi.java_rs_rawat_jalan.model.RoleResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+
+public interface PolyclinicService {
+
+  PolyclinicResponse create(User user, PolyclinicRequest request);
+
+  PolyclinicResponse get(User user, UUID id);
+
+  List<PolyclinicResponse> getAll(User user);
+
+  PolyclinicResponse update(User user, PolyclinicRequest request, UUID id);
+
+  void delete(User user, UUID id);
+}
